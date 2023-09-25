ef8a8a96f83bcfcb8124d8bdb958435d34d9fda7
cabrera-16 <108741462+cabrera-16@users.noreply.github.com>
2023-09-24 21:19:26 -0400

    trabajdo de mi laptop

--- js/Ejercio-1-10.js
+++ js/Ejercio-1-10.js
@@ -135,5 +135,8 @@ invetir_palabra("hola mundo","","");
  /****************************************************************************************************************** */
 /* 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, 
 pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.*/
-console.log("hola")
+const eliminar_patron=(texto="")=>{
+  if (!texto) return ("No ingresate el texto");
+
+}
 
