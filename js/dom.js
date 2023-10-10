14c861d86aac2607e2e0181f0a85cc0eed7c3e5a
mi primer git <josemiguelcabrera4@gmail.com>
2023-10-10 17:11:38 -0400

    nodo,atributo del DOM

---
+++ js/dom.js
@@ -0,0 +1,10 @@
+/*document.write("esto es javascripts");
+console.log(document.doctype);
+console.log(document.images);
+console.log (document.links);
+console.log(document.title);
+console.log(document.URL);
+console.log(document.forms);
+console.log(document.scripts);
+console.log(document.location)*/
+
