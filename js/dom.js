4759e3d5679a740808a76a1942b8e6f69c9bd21b
mi primer git <josemiguelcabrera4@gmail.com>
2023-10-12 16:29:17 -0400

    trabajando DOM

--- js/dom.js
+++ js/dom.js
@@ -16,7 +16,7 @@ console.log($menu);
 console.log($nav);
 console.log($figura);
 */
-
+/*
 const $linkDom = document.querySelector(".link-dom");
 console.log($linkDom.style);
 console.log($linkDom.getAttribute(""));
@@ -37,6 +37,74 @@ const $body=document.body;
 let varDakColor = getComputedStyle($html).getPropertyValue("--dark-color");
 let varYellow = getComputedStyle($html).getPropertyValue("--yelow-color");
 
-console.log(varDakColor,varYellow)
+console.log(varDakColor,varYellow);
+
 $body.style.backgroundColor=varDakColor
-$body.style.color = varYellow
+$body.style.color = varYellow;
+*/
+// clases en css
+const $card = document.querySelector(".card");
+console.log($card);
+console.log($card.className);
+console.log($card.classList);
+console.log($card.classList.contains("rotate-45"));
+//agregar una clase
+$card.classList.add("rotate-45");
+console.log($card.classList.contains("rotate-45"))
+//para saber nombre de la clase
+console.log($card.className);
+//lista de clase
+console.log($card.classList);
+// remove e para remover clase
+$card.classList.remove("rotate-45");
+//clase lista q contiene
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+const  $v = document.querySelector(".card2");
+console.log($v)
+console.log($v.className)
+$v.classList.add("rotate-135");
+console.log($v.className)
+console.log($v.classList);
+$v.classList.remove("rotate-135");
+console.log($v.className)
+// toggle q hace saca la classe esta hilden o escondida
+$v.classList.toggle("rotate-135");
+console.log($v.className);
+console.log($v.classList)
+
+//replazar una clase 
+$v.classList.replace('rotate-135','rotate-45');
+console.log($v);
+
+
+
+
+
+//console.log($card.children[2].toggle("rotate-135"))
+//fragmento
+mese=[
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre"
+]
+const $ul=document.createElement("ul");
+const $fragment = document.createDocumentFragment();
+mese.forEach((el)=>{
+ const $li =document.createElement("li");
+ $li.textContent =el;
+ $fragment.appendChild($li)
+})
+document.write("<h3>Meses del AñO</H3>");
+$ul.appendChild($fragment);
+document.body.append($ul);//aqui se agrega el fragment al body
+
